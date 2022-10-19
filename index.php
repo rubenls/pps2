f3298f3e4d85da0f2fcb3c2bf59dc98e063579ba
Ruben <rlasrod1807@iesmartinezm.es>
2022-10-19 21:41:08 +0200

    Commit sin archivo olvidado

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
-<?php echo"primer commit modificado 2" ?> 
+<?php echo"primer commit modificado 2 PARA DESHACER"
+echo "diferencia entre stage y workingarea" ?> 
---
+++ nuevo.php
@@ -0,0 +1,1 @@
+<?php echo"nuevo archivo" ?>
